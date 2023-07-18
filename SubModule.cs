86f613be70a6b167e48bcd8caee9f64c9edc878f
Kristoffer Dahl <krsylte@gmail.com>
2023-07-18 15:16:16 +0200

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,34 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+
+namespace ArenaImprovements
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public override void OnGameInitializationFinished(Game game)
+        {
+            base.OnGameInitializationFinished(game);
+            InformationManager.DisplayMessage(new InformationMessage("ArenaImprovements test project loaded", Color.ConvertStringToColor("#FF0042FF")));
+
+        }
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
